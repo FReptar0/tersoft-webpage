a13eb5441be98e73cbdf4d7d44cc3633a6b3b5d4
FReptar0 <20213tn137@utez.edu.mx>
2023-05-31 21:55:15 -0600

    Se agrega las etiquetas para que analytics funcione

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/global.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const MyApp = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics/>
+        </>
+    )
 };
 
 export default MyApp;
