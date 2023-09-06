912f7b0d48b9dc3cdbd75ace7e72468d96d08c74
FReptar0 <20213tn137@utez.edu.mx>
2023-09-05 21:34:20 -0600

    i18 for Hero component

--- components/Hero.js
+++ components/Hero.js
@@ -3,10 +3,24 @@ import { useColorModeValue } from '@chakra-ui/color-mode';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import ReCAPTCHA from 'react-google-recaptcha';
-import React, { useState, createRef } from 'react';
+import React, { useState, createRef, useEffect } from 'react';
 import Swal from 'sweetalert2';
 import axios from 'axios';
+import HeroTextsEN from '../public/langs/en/Hero.json';
+import HeroTextsES from '../public/langs/es/Hero.json';
+import Router from 'next/router';
+
 export default function CallToActionWithVideo() {
+    const [heroTexts, setHeroTexts] = useState(HeroTextsES);
+    useEffect(() => {
+        console.log(Router.locale);
+        if (Router.locale === 'en') {
+            setHeroTexts(HeroTextsEN);
+        } else {
+            setHeroTexts(HeroTextsES);
+        }
+    }, []);
+    console.log(heroTexts);
     return (
         <>
             <Container maxW={'7xl'}>
@@ -14,15 +28,15 @@ export default function CallToActionWithVideo() {
                     <Stack flex={1} spacing={{ base: 5, md: 10 }}>
                         <Heading as={'h1'} lineHeight={1.1} fontWeight={600} fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
                             <Text as={'span'} position={'relative'} _after={{ content: "''", width: 'full', height: '30%', position: 'absolute', bottom: 1, left: 0, bg: 'green.400', zIndex: -1 }}>
-                                Sage 300
+                                {heroTexts.h1}
                             </Text>
                             <br />
                             <Text as={'span'} color={'green.400'}>
-                                el mejor sistema ERP!
+                                {heroTexts.h1Underline}
                             </Text>
                         </Heading>
                         <Text color={'gray.800'} textAlign={'justify'}>
-                            Sage 300 antes conocido como Accpac, es la solución ERP integral para empresas en ascenso. Unifica contabilidad, compras, inventarios, ventas y más en un sistema centralizado, automatizando procesos y maximizando la eficiencia. En Tersoft, no solo implementamos Sage 300, sino que adaptamos y optimizamos la herramienta a tus necesidades enfrentando desafíos y asegurando el éxito de tu negocio.
+                            {heroTexts.description}
                         </Text>
                         <Stack spacing={{ base: 4, sm: 6 }} direction={{ base: 'column', sm: 'row' }}>
                             <Button rounded={'full'} size={'lg'} fontWeight={'normal'} px={6} colorScheme={'green'} bg={'green.400'} _hover={{ bg: 'green.500' }} onClick={
@@ -30,7 +44,7 @@ export default function CallToActionWithVideo() {
                                     document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
                                 }
                             }>
-                                Quiero más información
+                                {heroTexts.firstCTA}
                             </Button>
                             <Button rounded={'full'} size={'lg'} fontWeight={'normal'} px={6} colorScheme={'green'}
                                 bg={'green.400'} _hover={{ bg: 'green.500' }}
@@ -40,14 +54,14 @@ export default function CallToActionWithVideo() {
                                         Calendly.initPopupWidget({ url: 'https://calendly.com/tersoft' }); return false;
                                     }}
                             >
-                                ¡Agenda una cita ahora!
+                                {heroTexts.secondCTA}
                             </Button>
                         </Stack>
                     </Stack>
                     <Flex flex={1} justify={'center'} align={'center'} position={'relative'} w={'full'}>
                         <Blob w={'150%'} h={'120%'} position={'absolute'} top={'5%'} left={0} zIndex={-1} color={useColorModeValue('green.200', 'green.400')} />
                         <Box minH={'400px'} maxH={'auto'} rounded={'2xl'} boxShadow={'2xl'} width={'400px'} overflow={'hidden'}>
-                            <FormComponent />
+                            <FormComponent texts={heroTexts.form} />
                         </Box>
                     </Flex>
                 </Stack>
@@ -69,8 +83,8 @@ const Blob = (props) => {
     );
 };
 
-const FormComponent = () => {
-
+const FormComponent = (formTexts) => {
+    console.log(formTexts);
     const [telefono, setTelefono] = useState('');
     const [isDisabled, setIsDisabled] = useState(true);
     const [reCaptchaResponse, setReCaptchaResponse] = useState('');
@@ -90,8 +104,8 @@ const FormComponent = () => {
 
 
     const validationSchema = Yup.object({
-        name: Yup.string().required('Campo requerido'),
-        email: Yup.string().email('Formato de correo inválido').required('Campo requerido'),
+        name: Yup.string().required(),
+        email: Yup.string().email(formTexts.texts.validationSchema.emailInvalid).required(formTexts.texts.validationSchema.email),
     });
 
     const handleSubmit = async (values, { resetForm }) => {
@@ -107,8 +121,8 @@ const FormComponent = () => {
 
         if (!name || !email || !telefono) {
             Swal.fire({
-                title: 'Error',
-                text: 'Todos los campos son requeridos',
+                title: formTexts.texts.alerts.error.missingFields.title,
+                text: formTexts.texts.alerts.error.missingFields.description,
                 icon: 'error',
                 position: 'bottom-end',
                 showConfirmButton: false,
@@ -124,9 +138,9 @@ const FormComponent = () => {
 
             if (response.status === 200) {
                 Swal.fire({
-                    title: 'Correo electrónico enviado',
+                    title: formTexts.texts.alerts.success.title,
                     icon: 'success',
-                    text: 'Muchas gracias, su información ha sido enviada correctamente',
+                    text: formTexts.texts.alerts.success.description,
                     position: 'bottom-end',
                     showConfirmButton: false,
                     timer: 5000,
@@ -142,8 +156,8 @@ const FormComponent = () => {
                 });
             } else {
                 Swal.fire({
-                    title: 'Error',
-                    text: 'Lo sentimos, ha ocurrido un error al enviar su información, inténtelo más tarde',
+                    title: formTexts.texts.alerts.error.onSend.title,
+                    text: formTexts.texts.alerts.error.onSend.description,
                     icon: 'error',
                     position: 'bottom-end',
                     showConfirmButton: false,
@@ -162,8 +176,8 @@ const FormComponent = () => {
 
         } catch (error) {
             Swal.fire({
-                title: 'Error',
-                text: 'Lo sentimos, ha ocurrido un error al enviar su información, inténtelo más tarde',
+                title: formTexts.texts.alerts.error.onSend.title,
+                text: formTexts.texts.alerts.error.onSend.description,
                 icon: 'error',
                 position: 'bottom-end',
                 showConfirmButton: false,
@@ -196,7 +210,7 @@ const FormComponent = () => {
             shadow={'xl'}
         >
             <Heading as={'h2'} size={'md'} marginBottom={'30px'}>
-                ¡Aplique para una implementacion gratuita!
+                {formTexts.texts.heading}
             </Heading>
 
             <Formik
@@ -208,16 +222,16 @@ const FormComponent = () => {
                     <Form>
                         <div>
                             <Text as={'h3'} fontSize="md" textAlign="left" padding={'1'} marginBottom={'-0.1px'}>
-                                Nombre completo:
+                                {formTexts.texts.fields.labels.name}
                             </Text>
-                            <Field type="text" name="name" placeholder="Nombre" className="form-control mb-3" />
+                            <Field type="text" name="name" placeholder={formTexts.texts.fields.placeholders.name} className="form-control mb-3" />
                             <ErrorMessage name="name" component="div" className="text-danger" />
                         </div>
                         <div>
                             <Text as={'h3'} fontSize="md" textAlign="left" padding={'1'} marginBottom={'-0.1px'}>
-                                Número de teléfono:
+                                {formTexts.texts.fields.labels.phone}
                             </Text>
-                            <Field type="tel" name="phoneNumber" placeholder="Teléfono" className="form-control mb-3"
+                            <Field type="tel" name="phoneNumber" placeholder={formTexts.texts.fields.placeholders.phone} className="form-control mb-3"
                                 maxLength={10}
                                 minLength={10}
                                 inputMode="numeric"
@@ -225,14 +239,16 @@ const FormComponent = () => {
                                 value={telefono}
                             />
                             {telefono.length < 10 && telefono.length > 0 && (
-                                <span style={{ color: '#EB1111', fontSize: '1rem', marginLeft: 5 }}>El teléfono no es válido</span>
+                                <span style={{ color: '#EB1111', fontSize: '1rem', marginLeft: 5 }}>
+                                    {formTexts.texts.validationSchema.phoneInvalid}
+                                </span>
                             )}
                         </div>
                         <div>
                             <Text as={'h3'} fontSize="md" textAlign="left" padding={'1'} marginBottom={'-0.1px'}>
-                                Correo electrónico:
+                                {formTexts.texts.fields.labels.email}
                             </Text>
-                            <Field type="email" name="email" placeholder="Correo" className="form-control mb-3" />
+                            <Field type="email" name="email" placeholder={formTexts.texts.fields.placeholders.email} className="form-control mb-3" />
                             <ErrorMessage name="email" component="div" className="text-danger" />
                         </div>
 
@@ -274,7 +290,7 @@ const FormComponent = () => {
                             isDisabled={isDisabled}
                             isLoading={isSubmitting}
                         >
-                            ¡ Aplicar ahora !
+                            {formTexts.texts.fields.labels.send}
                         </Button>
                     </Form>
                 )}
