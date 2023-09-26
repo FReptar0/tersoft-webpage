60da6b5c06ae530ddaf1a08a93861cd3050412cf
FReptar0 <20213tn137@utez.edu.mx>
2023-09-26 11:35:05 -0600

    PurgeCSS Correction

--- postcss.config.js
+++ postcss.config.js
@@ -18,7 +18,6 @@ module.exports = {
             {
                 content: [
                     './pages/**/*.{js,jsx,ts,tsx}',
-                    './components/**/*.{js,jsx,ts,tsx}'
                 ],
                 defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
                 safelist: ["html", "body"]
